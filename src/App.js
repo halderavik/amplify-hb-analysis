1b997093c50bd4a1cba598e7296b8e4ed227ec54
haldevaik <halderavik@gmail.com>
2025-02-03 16:09:48 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ConjointApp from "./components/ConjointApp";
+
+function App() {
+  return (
+    <div>
+      <h1>Hierarchical Bayesian Conjoint Analysis</h1>
+      <ConjointApp />
+    </div>
+  );
+}
+
+export default App;
